6b13c7209f821cc837729786af89f995bf4e8638
devmetal <devmetal91@gmail.com>
2024-04-24 11:29:45 +0200

    ♻️ simplify the request. Using fe filtering on games

--- src/app/game-service.service.ts
+++ src/app/game-service.service.ts
@@ -10,7 +10,18 @@ import {
 } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { Game } from './types/game.type';
-import { Observable, filter, from, map, mergeMap } from 'rxjs';
+import {
+  Observable,
+  filter,
+  last,
+  from,
+  mergeMap,
+  map,
+  concatMap,
+  tap,
+  takeLast,
+  switchMap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -22,32 +33,22 @@ export class GameService {
   gamesColl = collection(this.firestore, 'games');
   gamesPlayersColl = collection(this.firestore, 'games_players');
 
-  getMyCurrentGame(): Observable<Game> {
+  getMyCurrentGame(): Observable<Game | undefined> {
     const me = this.authService.currentUserSig()!;
 
-    return collectionData(this.gamesPlayersColl).pipe(
+    return collectionData(this.gamesColl).pipe(
       map((records) => {
-        return (records as Array<{ playerId: string; gameId: string }>)
-          .filter((record) => record.playerId === me.id)
+        return (records as Game[])
+          .filter((record) => {
+            return record.players.some((player) => player.id === me.id);
+          })
           .at(-1);
-      }),
-      filter((record) => !!record),
-      mergeMap((record) => {
-        const promise = getDoc(
-          doc(this.firestore, `games/${record!.gameId}`)
-        ).then((snap) => snap.data());
-
-        return from(promise);
       })
-    ) as Observable<Game>;
+    );
   }
 
-  createGame(): Promise<void | boolean> {
-    const me = this.authService.currentUserSig();
-
-    if (!me) {
-      return this.router.navigateByUrl('/login');
-    }
+  createGame(): Promise<void> {
+    const me = this.authService.currentUserSig()!;
 
     const game: Game = {
       host: me.id,
